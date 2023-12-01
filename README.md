40d916a480e54eb5dabf093245b40d04ec524eef
wiidede <wiixdede@gmail.com>
2023-12-01 22:48:48 +0800

    feat: rename

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # UEditor Demo with Monaco
+
+You can edit rich text source more easily with [Monaco Editor](https://microsoft.github.io/monaco-editor/) and preview it in [UEditor](https://ueditor.baidu.com/).
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,7 +7,7 @@
     <a
       i-carbon-logo-github icon-btn
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/wiidede/UEditor-demo-Monaco"
       target="_blank"
       title="GitHub"
     />
